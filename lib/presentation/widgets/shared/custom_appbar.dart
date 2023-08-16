6fab0453776cea0704104fe37ed5e40c4d3a161c
Luis Leal <luisrrleal@gmail.com>
2023-08-15 20:33:13 -0600

    Add custom appbar

---
+++ lib/presentation/widgets/shared/custom_appbar.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final color = Theme.of(context).colorScheme.primary;
+
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: SizedBox(
+          width: double.infinity,
+          child: Row(
+            children: [
+              Icon(Icons.movie_outlined, color: color),
+              const SizedBox(width: 5),
+              Text(
+                'Cinemapedia',
+                style: TextStyle(
+                  color: color,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const Spacer(),
+              Icon(Icons.search, color: color),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
