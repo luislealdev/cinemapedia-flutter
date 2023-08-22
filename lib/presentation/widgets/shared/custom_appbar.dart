5bea3b02b54e5e4a1ea3bb3ab33774a46e4eff69
Luis Leal <luisrrleal@gmail.com>
2023-08-21 21:56:01 -0600

    Fix bug with custom appbar

--- lib/presentation/widgets/shared/custom_appbar.dart
+++ lib/presentation/widgets/shared/custom_appbar.dart
@@ -5,25 +5,20 @@ class CustomAppBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final color = Theme.of(context).colorScheme.primary;
+    final color = Theme.of(context).colorScheme.secondary;
+    final titleStyle = Theme.of(context).textTheme.titleMedium;
 
     return SafeArea(
+      bottom: false,
       child: Padding(
-        padding: const EdgeInsets.all(20),
+        padding: const EdgeInsets.symmetric(horizontal: 10),
         child: SizedBox(
           width: double.infinity,
           child: Row(
             children: [
               Icon(Icons.movie_outlined, color: color),
               const SizedBox(width: 5),
-              Text(
-                'Cinemapedia',
-                style: TextStyle(
-                  color: color,
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
+              Text("Cinemapedia", style: titleStyle),
               const Spacer(),
               Icon(Icons.search, color: color),
             ],
