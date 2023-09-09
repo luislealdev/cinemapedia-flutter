47c9860c0cd6b2431703127fdb3387c12f6f57e2
Luis Leal <luisrrleal@gmail.com>
2023-09-08 21:42:55 -0600

    Add provider and movie screen

--- lib/infrastructure/datasources/the_moviedb_datasource.dart
+++ lib/infrastructure/datasources/the_moviedb_datasource.dart
@@ -2,6 +2,7 @@ import 'package:cinemapedia/config/constants/enviroment.dart';
 import 'package:cinemapedia/domain/datasources/movies_datasource.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:cinemapedia/infrastructure/mappers/movie_mapper.dart';
+import 'package:cinemapedia/infrastructure/models/moviedb/movie_details.dart';
 import 'package:cinemapedia/infrastructure/models/moviedb/moviedb_response.dart';
 import 'package:dio/dio.dart';
 
@@ -46,5 +47,16 @@ class TheMoviedbDatasource extends MoviesDatasource {
     final response =
         await dio.get('/movie/upcoming', queryParameters: {'page': page});
     return jsonToMovieList(response.data);
+  }
+
+  @override
+  Future<Movie> getMovieById(String movieId) async {
+    final response = await dio.get('/movie/$movieId');
+
+    if(response.statusCode!=200) throw Exception('Movie with id: $movieId not found');
+
+    final movieDb = MovieDetails.fromJson(response.data);
+    final Movie movie = MovieMapper.movieDetailsToEntity(movieDb);
+    return movie;
   }
 }
--- lib/infrastructure/mappers/movie_mapper.dart
+++ lib/infrastructure/mappers/movie_mapper.dart
@@ -1,4 +1,5 @@
 import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/infrastructure/models/moviedb/movie_details.dart';
 import 'package:cinemapedia/infrastructure/models/moviedb/movie_from_moviedb.dart';
 
 class MovieMapper {
@@ -8,6 +9,26 @@ class MovieMapper {
           ? "https://image.tmdb.org/t/p/w500/${movieDB.backdropPath}"
           : "https://sd.keepcalms.com/i-w600/keep-calm-poster-not-found.jpg",
       genreIds: movieDB.genreIds.map((e) => e.toString()).toList(),
+      id: movieDB.id,
+      originalLanguage: movieDB.originalLanguage,
+      originalTitle: movieDB.originalTitle,
+      overview: movieDB.overview,
+      popularity: movieDB.popularity,
+      posterPath: movieDB.posterPath != ''
+          ? "https://image.tmdb.org/t/p/w500/${movieDB.posterPath}"
+          : "no-poster",
+      releaseDate: movieDB.releaseDate,
+      title: movieDB.title,
+      video: movieDB.video,
+      voteAverage: movieDB.voteAverage,
+      voteCount: movieDB.voteCount);
+
+  static Movie movieDetailsToEntity(MovieDetails movieDB) => Movie(
+      adult: movieDB.adult,
+      backdropPath: movieDB.backdropPath != ''
+          ? "https://image.tmdb.org/t/p/w500/${movieDB.backdropPath}"
+          : "https://sd.keepcalms.com/i-w600/keep-calm-poster-not-found.jpg",
+      genreIds: movieDB.genres.map((e) => e.name).toList(),
       id: movieDB.id,
       originalLanguage: movieDB.originalLanguage,
       originalTitle: movieDB.originalTitle,
